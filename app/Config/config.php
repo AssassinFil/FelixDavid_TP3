63ff697ebaf2a48aac89ead1a1bc27b581220af0
AssassinFil <felixdav_@hotmail.com>
2014-12-22 05:31:41 -0500

    first

---
+++ app/Config/config.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = array(
+	'Application' => array(
+		'name' 	  => 'CakeStrap',
+		'version' => 'v0.7',
+		'status'  => 1,
+		'maintenance' => 0
+	),
+	'Meta' => array(
+		'title' 	  => 'Travail Pratique 3',
+		'description' => 'root',
+		'keywords' 	  => 'root',
+	),
+	
+	'Google' => array(
+		'analytics'  => '',
+	),
+	
+	'Email' => array(
+		'from_email' => array('{{from_name}}' => '{{from_email}}'),
+		'contact_mail' => array('{{contact_name}}' => '{{contact_mail}}')
+	)
+);
+
+?>
